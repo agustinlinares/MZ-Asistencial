c0b4ad8b1c6f6fc1ce871eea51e9147d2b7346c2
marcos.jimenez <marcos.jimenez@mercanza.net>
2026-04-10 14:00:52 +0200

    comentar una linea

--- mzasistencial.client/src/pages/Admin/ICG/ICGCentrosPropios.jsx
+++ mzasistencial.client/src/pages/Admin/ICG/ICGCentrosPropios.jsx
@@ -6,7 +6,7 @@ import { saveAs } from 'file-saver-es';
 import { exportDataGrid } from 'devextreme/excel_exporter';
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import FichaCliente from './FichaCliente'; // ajusta la ruta según donde esté el archivo
+//import FichaCliente from './FichaCliente'; // ajusta la ruta según donde esté el archivo
 
 import DataGrid, {
     Column,
