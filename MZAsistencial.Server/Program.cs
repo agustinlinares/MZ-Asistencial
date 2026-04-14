314540b9dbcca5d0315fefc2c43081905f5b02c3
Adriana <adrblagar@gmail.com>
2026-04-14 10:48:16 +0200

    Actualizado Program.cs

--- MZAsistencial.Server/Program.cs
+++ MZAsistencial.Server/Program.cs
@@ -4,10 +4,8 @@ using MZAsistencial.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IDescuadresService, DescuadresService>();
@@ -21,7 +19,6 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
