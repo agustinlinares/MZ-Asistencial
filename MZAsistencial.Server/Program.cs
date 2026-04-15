f0cec52262740b41fe21e6970503e971bd56562e
Adriana <adrblagar@gmail.com>
2026-04-15 11:58:17 +0200

    Módulo Descuadres completado - frontend conectado a API

--- MZAsistencial.Server/Program.cs
+++ MZAsistencial.Server/Program.cs
@@ -4,17 +4,28 @@ using MZAsistencial.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("https://localhost:60007")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IDescuadresService, DescuadresService>();
 
-// Conexión a la base de datos
 builder.Services.AddDbContext<MZAsistencialContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IDescuadresService, DescuadresService>();
+
 var app = builder.Build();
+
+app.UseCors("AllowFrontend");
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
@@ -26,11 +37,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
-
 app.Run();
