0050b40e386026e1ebe9aa21b097c37241f1b699
Ayoubito04 <ayoubarramdani091@gmail.com>
2026-04-21 16:07:07 +0200

    Se pueden subir las plantillas de acuerdo

--- mzasistencial.client/vite.config.js
+++ mzasistencial.client/vite.config.js
@@ -56,7 +56,7 @@ export default defineConfig({
     server: {
         proxy: {
             '^/api': {
-                target: 'http://localhost:5118', 
+                target: 'https://127.0.0.1:7132', 
                 secure: false,                   
                 changeOrigin: true
             }
