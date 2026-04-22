c73d5d979a7b9b94cad1ae0001db1252d4c0e7e8
andreahidara <andreahidalgoarana@gmail.com>
2026-04-22 11:35:27 +0200

    Eliminar entrada Control Centros Concertado Duplicados del menú

--- mzasistencial.client/src/routes/AppRoutes.jsx
+++ mzasistencial.client/src/routes/AppRoutes.jsx
@@ -22,7 +22,6 @@ const CentrosPropios = lazy(() => import("@pages/Admin/Centros/CentrosPropios"))
 const CentrosConcertados = lazy(() => import("@pages/Admin/Centros/CentrosConcertados"));
 const Conciertos = lazy(() => import("@pages/Admin/Centros/Conciertos"));
 const Fincas = lazy(() => import("@pages/Admin/Centros/Fincas"));
-const ControlDuplicados = lazy(() => import("@pages/Admin/Centros/ControlDuplicados"));
 
 const ICGCentrosPropios = lazy(() => import("@pages/Admin/ICG/ICGCentrosPropios"));
 const ICGPlantillasICG = lazy(() => import("@pages/Admin/ICG/ICGPlantillasICG"));
@@ -88,7 +87,6 @@ const AppRoutes = () => {
                             <Route path="CentrosConcertados" element={<CentrosConcertados />} />
                             <Route path="Conciertos" element={<Conciertos />} />
                             <Route path="Fincas" element={<Fincas />} />
-                            <Route path="ControlDuplicados" element={<ControlDuplicados />} />
                         </Route>
 
                         <Route path="ICG">
