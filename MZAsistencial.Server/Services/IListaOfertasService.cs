d9d781e0ed6a8d86d76d4fbc6ded3c5f31fc8b3c
Adriana <adrblagar@gmail.com>
2026-04-23 13:58:03 +0200

    feat: Lista Ofertas - Centro resuelto para CentrosPropios y CentrosConcertados

---
+++ MZAsistencial.Server/Services/IListaOfertasService.cs
@@ -0,0 +1,10 @@
+﻿using MZAsistencial.Server.DTOs;
+
+namespace MZAsistencial.Server.Services;
+
+public interface IListaOfertasService
+{
+    Task<IEnumerable<ListaOfertasDTO>> GetListaOfertasAsync(FiltrosListaOfertasDTO filtros, int mutuaId);
+    Task<IEnumerable<object>> GetEstadosAsync();
+    Task<IEnumerable<int>> GetAñosAsync();
+}
