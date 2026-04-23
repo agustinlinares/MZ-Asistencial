6b425fe0eb895a3277732e9cc263272e32d39d91
Adriana <adrblagar@gmail.com>
2026-04-23 13:58:03 +0200

    feat: Lista Ofertas - Centro resuelto para CentrosPropios y CentrosConcertados

---
+++ MZAsistencial.Server/Controllers/ListaOfertasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MZAsistencial.Server.DTOs;
+using MZAsistencial.Server.Services;
+
+namespace MZAsistencial.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ListaOfertasController : ControllerBase
+{
+    private readonly IListaOfertasService _service;
+
+    public ListaOfertasController(IListaOfertasService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost("lista")]
+    public async Task<IActionResult> GetLista(
+        [FromBody] FiltrosListaOfertasDTO filtros,
+        [FromQuery] int mutuaId = 1)  
+    {
+        var result = await _service.GetListaOfertasAsync(filtros, mutuaId);
+        return Ok(result);
+    }
+
+    [HttpGet("estados")]
+    public async Task<IActionResult> GetEstados()
+    {
+        var result = await _service.GetEstadosAsync();
+        return Ok(result);
+    }
+
+    [HttpGet("años")]
+    public async Task<IActionResult> GetAños()
+    {
+        var result = await _service.GetAñosAsync();
+        return Ok(result);
+    }
+}
