2f643aa6c62f1d45bb4b4801f264c4f16860c3db
Ayoubito04 <ayoubarramdani091@gmail.com>
2026-05-28 02:08:58 +0200

    Se pueden agregar ficheros correctamente en la parte de export acces

--- MZAsistencial.Server/Services/ExportarAccessService.cs
+++ MZAsistencial.Server/Services/ExportarAccessService.cs
@@ -69,10 +69,18 @@ namespace MZAsistencial.Server.Services
 
         public async Task<IEnumerable<int>> GetAñosAsync()
         {
-            return await _context.Ejercicios
+            var años = await _context.Ejercicios
                 .OrderByDescending(e => e.Año)
                 .Select(e => e.Año)
                 .ToListAsync();
+
+            if (años.Count == 0)
+            {
+                var actual = DateTime.Now.Year;
+                años = Enumerable.Range(actual - 5, 7).OrderByDescending(a => a).ToList();
+            }
+
+            return años;
         }
 
         public async Task<FicheroGeneradoDTO> CreateAsync(int mutuaIntId, int año, int tipoCentroId, int usuarioId)
