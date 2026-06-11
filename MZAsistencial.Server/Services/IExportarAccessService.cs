e20fd87d1806055b12a3d63bd4082157dcfaa084
Ayoubito04 <ayoubarramdani091@gmail.com>
2026-06-11 17:49:28 +0200

    Cambios dentro de export access con filtrado de ficheros por perfil(perfil 1 y 2)

--- MZAsistencial.Server/Services/IExportarAccessService.cs
+++ MZAsistencial.Server/Services/IExportarAccessService.cs
@@ -5,7 +5,7 @@ namespace MZAsistencial.Server.Services
 {
     public interface IExportarAccessService
     {
-        Task<IEnumerable<FicheroGeneradoDTO>> GetAllAsync();
+        Task<IEnumerable<FicheroGeneradoDTO>> GetAllAsync(int? mutuaId = null);
         Task<IEnumerable<Mutua>> GetMutuasAsync();
         Task<IEnumerable<int>> GetAñosAsync();
         Task<FicheroGeneradoDTO> CreateAsync(int mutuaIntId, int año, int tipoCentroId, int usuarioId);
