acf579a08d6787ac9aad8ba1a773adc588b016d8
Ayoubito04 <ayoubarramdani091@gmail.com>
2026-06-11 17:49:28 +0200

    Cambios dentro de export access con filtrado de ficheros por perfil(perfil 1 y 2)

--- MZAsistencial.Server/Controllers/ExportarAccessController.cs
+++ MZAsistencial.Server/Controllers/ExportarAccessController.cs
@@ -15,9 +15,9 @@ namespace MZAsistencial.Server.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] int? mutuaId = null)
         {
-            var data = await _service.GetAllAsync();
+            var data = await _service.GetAllAsync(mutuaId);
             return Ok(data);
         }
 
